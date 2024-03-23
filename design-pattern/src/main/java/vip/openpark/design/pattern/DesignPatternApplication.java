26d78c3a61845174af7b13e7c99450ac580c8f87
openpark <openpark.vip@outlook.com>
2024-03-23 22:12:49 +0800

    build：添加关于多线程设计模式的模块

---
+++ design-pattern/src/main/java/vip/openpark/design/pattern/DesignPatternApplication.java
@@ -0,0 +1,13 @@
+package vip.openpark.design.pattern;
+
+/**
+ * 多线程设计模式
+ *
+ * @author anthony
+ * @since 2024/3/23 22:11
+ */
+public class DesignPatternApplication {
+	public static void main(String[] args) {
+	
+	}
+}
