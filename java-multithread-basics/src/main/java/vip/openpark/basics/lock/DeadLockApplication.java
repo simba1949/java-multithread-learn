955a1b7bcb597931538c6655fab022deefdbae22
anthony <anthony1949@zto.com>
2024-03-23 10:05:50 +0800

    style：思索添加必要注释

--- java-multithread-basics/src/main/java/vip/openpark/basics/lock/DeadLockApplication.java
+++ java-multithread-basics/src/main/java/vip/openpark/basics/lock/DeadLockApplication.java
@@ -8,46 +8,48 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class DeadLockApplication {
-	public static void main(String[] args) {
-		Object a = new Object();
-		Object b = new Object();
-		
-		Thread t1 = new Thread(() -> {
-			synchronized (a) {
-				log.info("lock a");
-				try {
-					Thread.sleep(10);
-					synchronized (b) {
-						log.info("lock b");
-					}
-				} catch (InterruptedException e) {
-					log.info("InterruptedException", e);
-				}
-			}
-		}, "t1");
-		
-		Thread t2 = new Thread(() -> {
-			synchronized (b) {
-				log.info("lock b");
-				try {
-					Thread.sleep(5);
-					synchronized (a) {
-						log.info("lock a");
-					}
-				} catch (InterruptedException e) {
-					log.info("InterruptedException", e);
-				}
-			}
-		}, "t2");
-		
-		t1.start();
-		t2.start();
-		
-		try {
-			// 等待t1和t2线程启动
-			Thread.sleep(2_000);
-		} catch (InterruptedException e) {
-			log.info("InterruptedException", e);
-		}
-	}
+    public static void main(String[] args) {
+        Object lockA = new Object();
+        Object lockB = new Object();
+
+        Thread t1 = new Thread(() -> {
+            synchronized (lockA) {
+                log.info("lock a");
+                try {
+                    // 线程睡眠，目的是等待线程2启动并拿到锁
+                    Thread.sleep(2_000);
+                    synchronized (lockB) {
+                        log.info("lock b");
+                    }
+                } catch (InterruptedException e) {
+                    log.info("InterruptedException", e);
+                }
+            }
+        }, "t1");
+
+        Thread t2 = new Thread(() -> {
+            synchronized (lockB) {
+                log.info("lock b");
+                try {
+                    // 线程睡眠，目的是等待线程1启动并拿到锁
+                    Thread.sleep(2_000);
+                    synchronized (lockA) {
+                        log.info("lock a");
+                    }
+                } catch (InterruptedException e) {
+                    log.info("InterruptedException", e);
+                }
+            }
+        }, "t2");
+
+        t1.start();
+        t2.start();
+
+        try {
+            // 等待t1和t2线程启动
+            Thread.sleep(2_000);
+        } catch (InterruptedException e) {
+            log.info("InterruptedException", e);
+        }
+    }
 }
